b213f393ea82d97dbc19b9c07dae7570a8f9072a
phenriquep00 <phenriquep00@gmail.com>
2024-01-30 19:32:58 -0300

    finished authorization flow

--- src/main/java/com/phenriquep00/musictrackerapi/auth/AuthController.java
+++ src/main/java/com/phenriquep00/musictrackerapi/auth/AuthController.java
@@ -3,19 +3,21 @@ package com.phenriquep00.musictrackerapi.auth;
 import java.io.IOException;
 import java.net.URI;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.SpotifyHttpManager;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 
-import javax.annotation.PostConstruct;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -50,6 +52,35 @@ public class AuthController {
                 .build();
 
         final URI uri = authorizationCodeUriRequest.execute();
+        
         return uri.toString();
+    }
+
+    @GetMapping("/get-user-code/")
+    public String getSpotifyUserCode(@RequestParam("code") String userCode, HttpServletResponse response) throws IOException
+    {
+        code = userCode;
+
+        AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code)
+            .build();
+
+        try 
+        {
+            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+
+            // Set access and refresh token for further "spotifyApi" object usage
+            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
+            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+
+            System.out.println("Expires in: " + authorizationCodeCredentials.getExpiresIn());
+        }
+        catch (IOException | SpotifyWebApiException | org.apache.hc.core5.http.ParseException e) 
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        response.sendRedirect("http://localhost:4200/home");
+
+        return spotifyApi.getAccessToken();
     }
 }
