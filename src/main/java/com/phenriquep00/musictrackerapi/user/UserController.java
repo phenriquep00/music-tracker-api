2cc0cdd26b8b883a60a817b3b70d83dd101790c8
phenriquep00 <phenriquep00@gmail.com>
2024-02-29 18:42:30 -0300

    created endpoint get-user-recent-albums to substitute the getUserSavedAlbums method, in order to return only information needed for the AlbumDTO

--- src/main/java/com/phenriquep00/musictrackerapi/user/UserController.java
+++ src/main/java/com/phenriquep00/musictrackerapi/user/UserController.java
@@ -1,16 +1,10 @@
 package com.phenriquep00.musictrackerapi.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import se.michaelthelin.spotify.SpotifyApi;
-import se.michaelthelin.spotify.model_objects.specification.Artist;
-import se.michaelthelin.spotify.model_objects.specification.Paging;
-import se.michaelthelin.spotify.model_objects.specification.SavedAlbum;
-import se.michaelthelin.spotify.requests.data.library.GetCurrentUsersSavedAlbumsRequest;
-import se.michaelthelin.spotify.requests.data.personalization.simplified.GetUsersTopArtistsRequest;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
