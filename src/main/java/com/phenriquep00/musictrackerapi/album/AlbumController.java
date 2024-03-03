2058092d8370e5e6720f00a91cdc2ae5ee93d182
phenriquep00 <phenriquep00@gmail.com>
2024-03-03 17:26:01 -0300

    created service to populate a album page + trackDTO

--- src/main/java/com/phenriquep00/musictrackerapi/album/AlbumController.java
+++ src/main/java/com/phenriquep00/musictrackerapi/album/AlbumController.java
@@ -1,22 +1,26 @@
 package com.phenriquep00.musictrackerapi.album;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.specification.Album;
 import se.michaelthelin.spotify.model_objects.specification.Artist;
 import se.michaelthelin.spotify.model_objects.specification.Paging;
 import se.michaelthelin.spotify.model_objects.specification.SavedAlbum;
+import se.michaelthelin.spotify.requests.data.albums.GetAlbumRequest;
 import se.michaelthelin.spotify.requests.data.library.GetCurrentUsersSavedAlbumsRequest;
 import se.michaelthelin.spotify.requests.data.personalization.simplified.GetUsersTopArtistsRequest;
 
@@ -58,6 +62,23 @@ public class AlbumController {
         .collect(Collectors.toList());
 
       return ResponseEntity.ok(userRecentAlbums);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+  }
+
+  @GetMapping("/get-album/{albumId}")
+  public ResponseEntity<AlbumDTO> getAlbumById(@PathVariable String albumId) {
+    final GetAlbumRequest getAlbumRequest = spotifyApi
+      .getAlbum(albumId)
+      .build();
+
+    try {
+      Album response = getAlbumRequest.execute();
+      AlbumDTO album = new AlbumDTO(response);
+      
+      return ResponseEntity.ok(album);
+
     } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
--- src/main/java/com/phenriquep00/musictrackerapi/album/AlbumDTO.java
+++ src/main/java/com/phenriquep00/musictrackerapi/album/AlbumDTO.java
@@ -1,5 +1,13 @@
 package com.phenriquep00.musictrackerapi.album;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.phenriquep00.musictrackerapi.track.TrackDTO;
+
 import lombok.Data;
 import se.michaelthelin.spotify.model_objects.specification.*;
 
@@ -11,6 +19,8 @@ public class AlbumDTO {
     private ArtistSimplified[] artists; 
     private String[] genres; 
     private Image[] images;
+    private String year;
+    private List<TrackDTO> tracks;
 
     public AlbumDTO(SavedAlbum savedAlbum) {
         this.id = savedAlbum.getAlbum().getId();
@@ -18,5 +28,25 @@ public class AlbumDTO {
         this.artists = savedAlbum.getAlbum().getArtists();
         this.genres = savedAlbum.getAlbum().getGenres();
         this.images = savedAlbum.getAlbum().getImages();
+    }
+
+    public AlbumDTO(Album album) {
+        this.id = album.getId();
+        this.albumName = album.getName();
+        this.artists = album.getArtists();
+        this.genres = album.getGenres();
+        this.images = album.getImages();
+        this.year = album.getReleaseDate();
+        this.tracks = convertTrackSimplifiedToTrackDTO(album.getTracks().getItems());
+    }
+
+    public List<TrackDTO> convertTrackSimplifiedToTrackDTO(TrackSimplified[] inputTracksSimplified) {
+        List<TrackSimplified> tracks = Arrays.asList(inputTracksSimplified);
+
+        List<TrackDTO> parsedTracks = tracks.stream()
+                .map(TrackDTO::new)
+                .collect(Collectors.toList());
+
+        return parsedTracks;
     }
 }
